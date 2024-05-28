3ce607e5e840bab2c207c9bd2d01bad1008ec547
ElioBoyy <mathis.sportiello@gmail.com>
2024-05-28 15:01:35 +0200

    on repart de 0 pour le front

--- components/notConnected/NotConnected.tsx
+++ components/notConnected/NotConnected.tsx
@@ -1,34 +1,9 @@
-'use client'
 
-import { Button } from "@nextui-org/button";
-import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/modal"
 
-export const NotConnected = () => {
-    const { onOpenChange } = useDisclosure();
-    const isOpen = true;
-    
+export default function NotConnected() {
     return (
-        <>
-            <Modal isOpen={isOpen} backdrop="blur" onOpenChange={onOpenChange}>
-                <ModalContent>
-                {(onClose) => (
-                    <>
-                        <ModalHeader className="flex flex-col gap-1">{'Vous n\'êtes pas identifié'}</ModalHeader>
-                        <ModalBody>
-                            <p className="text-default-900">Vous devez être connecté pour accéder à cette page.</p>
-                        </ModalBody>
-                        <ModalFooter className="flex justify-between">
-                            <Button className="w-fit" color="primary" variant="bordered" onClick={(e:any) => window.location.href = '/login'}>
-                                Connexion
-                            </Button>
-                            <Button className="w-fit text-white" color="primary" onClick={(e:any) => window.location.href = '/'}>
-                                {'Page d\'accueil'}
-                            </Button>
-                        </ModalFooter>
-                    </>
-                )}
-            </ModalContent>
-            </Modal>
-        </>
-    )
+        <div>
+            <h1>Not Connected</h1>
+        </div>
+    );
 }
