ff926bc5d91c58de6cc69d19c235d931fd889cab
Matheo <garerimath73660@gmail.com>
2024-06-21 15:00:21 +0200

    idem

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import Header from "@/components/header/Header";
 import { cn } from "@/lib/utils";
 import { SocketProvider } from "@/components/provider/socket-provider";
 import { GlobaleProvider } from "@/components/provider/globale-provider";
-import { QueryProvider } from "@/components/provider/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,16 +28,14 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <QueryProvider>
-            <GlobaleProvider>
-              <SocketProvider>
-                <Header />
-                <div className="absolute pt-[100px] w-full h-full">
-                  {children}
-                </div>
-              </SocketProvider>
-            </GlobaleProvider>
-          </QueryProvider>
+          <GlobaleProvider>
+            <SocketProvider>
+              <Header />
+              <div className="absolute pt-[100px] w-full h-full">
+                {children}
+              </div>
+            </SocketProvider>
+          </GlobaleProvider>
         </ThemeProvider>
       </body>
     </html>
